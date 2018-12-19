444ddb6de1218911883ceed33f009062ba11ca03
Piotr Wieczorek <piotrek@cs.uni.wroc.pl>
2018-12-19 08:49:14 +0100

    dodano komentarz

--- quicksort.pl
+++ quicksort.pl
@@ -8,6 +8,7 @@ project_dedalus_quicksort([X|Xs],Ys) :-
   project_dedalus_quicksort(Right,Rs),
   append(Ls,[X|Rs],Ys).
 
+% tu chyba miałem coś dodać
 project_dedalus_quicksort_helper_partition([X|Xs],Y,[X|Ls],Rs) :- 
   X =< Y, 
   project_dedalus_quicksort_helper_partition(Xs,Y,Ls,Rs).
